70bde1db813f808e9334022b0e7b01c083ee4154
Anantha Krishnan <aabros2003@gmail.com>
2022-10-15 12:17:27 +0530

    Initial COmmit

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    if(N == 1){
+        cout << "One" << endl;
+    }
+    else if(N == 2){
+        cout << "Two" << endl;
+    }
+    else if(N == 3){
+        cout << "Three" << endl;
+    }
+    else if(N == 4){
+        cout << "Four" << endl;
+    }
+    else if(N == 5){
+        cout << "Five" << endl;
+    }
+    else if(N == 6){
+        cout << "Six" << endl;
+    }
+    else if(N == 7){
+        cout << "Seven" << endl;
+    }
+    else if(N == 8){
+        cout << "Eight" << endl;
+    }
+    else if(N == 9){
+        cout << "Nine" << endl;
+    }
+    else{
+        cout << "Greater than 9" << endl;
+    }
+}
